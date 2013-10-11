8186922fa099e5dcb50b2dc5d2b950d49361c3ae
Kalen Jordan <kalen@sparcksoft.com>
2013-10-11 11:48:34 -0700

    Change autocomplete.php to admin-global-search-autocomplete.php

--- app/code/community/Clean/ElasticSearch/Block/Page/Header.php
+++ app/code/community/Clean/ElasticSearch/Block/Page/Header.php
@@ -5,7 +5,7 @@ class Clean_ElasticSearch_Block_Page_Header extends  Mage_Adminhtml_Block_Page_H
     public function getUrl($route = '', $params = array())
     {
         if ($route == 'adminhtml/index/globalSearch') {
-            $url = Mage::getStoreConfig('web/secure/base_url') . 'autocomplete.php';
+            $url = Mage::getStoreConfig('web/secure/base_url') . 'admin-global-search-autocomplete.php';
             $url .= '?base_url=' . urlencode(Mage::helper('adminhtml')->getUrl('adminhtml'));
 
             return $url;
